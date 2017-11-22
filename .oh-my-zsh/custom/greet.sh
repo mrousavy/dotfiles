aa0c029415acad213dee4a4efde75c353ba6e487
Marc Rousavy <marcrousavy@hotmail.com>
2017-11-22 15:07:35 +0100

    Add custom greet script

---
+++ .oh-my-zsh/custom/greet.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+text="Good morning, $(whoami)."
+length=${#text}
+
+half=$(tput cols)
+half=$(($half / 2 - $length / 2))
+i="0"
+echo ""		# Make newline at start
+echo ""		# Make newline at start
+
+
+while [ $i -lt $half ]
+do
+	printf " "
+	i=$[$i+1]
+done
+
+echo "$indent $text"
