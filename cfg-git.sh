11960a040fd66c9bce3d76997c642d180cbaff18
Marc Rousavy <marcrousavy@hotmail.com>
2017-11-24 14:03:14 +0100

    Add cfg-git dotfiles script for updating dotfiles repo

---
+++ cfg-git.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Copy .configs to git dotconfig
+cp ~/.config/kwinrc ~/Documents/Projects/dotfiles/.config/kwinrc
+cp ~/.zshrc ~/Documents/Projects/dotfiles/.zshrc
+cp ~/.vimrc ~/Documents/Projects/dotfiles/.vimrc
+cp ~/.oh-my-zsh/custom/greet.sh ~/Documents/Projects/dotfiles/.oh-my-zsh/custom/greet.sh
+cp ~/.oh-my-zsh/custom/keys.sh ~/Documents/Projects/dotfiles/.oh-my-zsh/custom/keys.sh
+cp ~/.local/share/konsole/zsh.profile ~/Documents/Projects/dotfiles/.local/share/konsole/zsh.profile
+
