21c10b50cc3b0f0ee5191865a8d9513034a33120
Marc Rousavy <marcrousavy@hotmail.com>
2017-11-30 08:18:00 +0100

    Update configs from linux script

--- install.sh
+++ install.sh
@@ -45,8 +45,8 @@ cp .vimrc ~/.vimrc
 #### VIM PLUGINS ####
 
 # Pathogen Vim
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # NERDTree
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
