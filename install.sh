281d1961a8bcc846138c14fcc520fb8c19c82c66
Marc Rousavy <marcrousavy@hotmail.com>
2018-01-09 10:22:16 +0100

    Ask Y/N before every config install

--- install.sh
+++ install.sh
@@ -12,78 +12,97 @@ apt update
 apt upgrade
 
 # Install Reqired packages
-apt install curl wmctrl git build-essential cmake python3 python3-dev python3-pip #zsh
+read -p "Do you want to install required packages via apt? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    apt install curl wmctrl git build-essential cmake python3 python3-dev python3-pip zsh
+fi
 
 #### ZSH ####
 
-# Install Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+read -p "Do you want to install Zsh themes, scripts and plugins? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # Install Oh-My-Zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Zsh-Autosuggestions
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH/custom/plugins/zsh-autosuggestions
+    # Zsh-Autosuggestions
+    git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH/custom/plugins/zsh-autosuggestions
 
-# Zsh-Syntax Highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
+    # Zsh-Syntax Highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
 
-# Zsh-Bullet Train theme
-mkdir $ZSH/custom/themes
-curl https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme -o $ZSH/custom/themes/bullet-train.zsh-theme
+    # Zsh-Bullet Train theme
+    mkdir $ZSH/custom/themes
+    curl https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme -o $ZSH/custom/themes/bullet-train.zsh-theme
 
-# Backup old .zshrc
-mv ~/.zshrc ~/.zshrc_backup
-# Copy over .zshrc
-cp .zshrc ~/.zshrc
-# Update for now (only required if using zsh)
-source ~/.zshrc
+    # Backup old .zshrc
+    mv ~/.zshrc ~/.zshrc_backup
+    # Copy over .zshrc
+    cp .zshrc ~/.zshrc
+    # Update for now (only required if using zsh)
+    source ~/.zshrc
 
-# Backup old .vimrc
-mv ~/.vimrc ~/.vimrc_backup
-# Copy over .vimrc
-cp .vimrc ~/.vimrc
-
+    # Backup old .vimrc
+    mv ~/.vimrc ~/.vimrc_backup
+    # Copy over .vimrc
+    cp .vimrc ~/.vimrc
+fi
 
 #### VIM PLUGINS ####
 
-# Pathogen Vim
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+read -p "Do you want to install Vim configuration, plugins and themes? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # Pathogen Vim
+    mkdir -p ~/.vim/autoload ~/.vim/bundle
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# NERDTree
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+    # NERDTree
+    git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
-# YouCompleteMe
-cd ~/.vim/bundle
-git clone https://github.com/Valloric/YouCompleteMe.git
-cd YouCompleteMe
-git submodule update --init --recursive
-./install.py --clang-completer
+    # YouCompleteMe
+    cd ~/.vim/bundle
+    git clone https://github.com/Valloric/YouCompleteMe.git
+    cd YouCompleteMe
+    git submodule update --init --recursive
+    ./install.py --clang-completer
 
-# SurroundMe
-cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-surround.git
+    # SurroundMe
+    cd ~/.vim/bundle
+    git clone git://github.com/tpope/vim-surround.git
 
-# FugitiveVim (Git)
-cd ~/.vim/bundle
-git clone https://github.com/tpope/vim-fugitive.git
+    # FugitiveVim (Git)
+    cd ~/.vim/bundle
+    git clone https://github.com/tpope/vim-fugitive.git
 
-# Vim Powerline Theme
-cd ~/.vim/bundle
-git clone https://github.com/lokaltog/vim-powerline
+    # Vim Powerline Theme
+    cd ~/.vim/bundle
+    git clone https://github.com/lokaltog/vim-powerline
+fi
+
 
 #### KDE ####
 
-# Copy over custom scripts
-cp .oh-my-zsh/custom/greet.sh ~/.oh-my-zsh/custom/greet.sh
-cp .oh-my-zsh/custom/keys.sh ~/.oh-my-zsh/custom/keys.sh
-cp run-focus /usr/local/bin/run-focus && chmod a+x /usr/local/bin/run-focus
+read -p "Do you want to install KDE, Konsole and KWin configurations and scripts? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # Copy over custom scripts
+    cp .oh-my-zsh/custom/greet.sh ~/.oh-my-zsh/custom/greet.sh
+    cp .oh-my-zsh/custom/keys.sh ~/.oh-my-zsh/custom/keys.sh
+    cp run-focus /usr/local/bin/run-focus && chmod a+x /usr/local/bin/run-focus
 
-# Copy over Konsole settings [KDE]
-cp .local/share/konsole/zsh.profile ~/.local/share/konsole/zsh.profile
-cp .config/konsolerc ~/.config/konsolerc
+    # Copy over Konsole settings [KDE]
+    cp .local/share/konsole/zsh.profile ~/.local/share/konsole/zsh.profile
+    cp .config/konsolerc ~/.config/konsolerc
 
-# Copy over KDE
-cp .kde/share/config/kdeglobals ~/.kde/share/config/kdeglobals
+    # Copy over KDE
+    cp .kde/share/config/kdeglobals ~/.kde/share/config/kdeglobals
 
-# Copy over KWin settings [KDE]
-cp .config/kwinrc ~/.config/kwinrc
-
+    # Copy over KWin settings [KDE]
+    cp .config/kwinrc ~/.config/kwinrc
+fi
