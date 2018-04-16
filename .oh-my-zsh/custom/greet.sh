62092bb393d3b499e96ed3fe2215569a67e77323
Marc Rousavy <marcrousavy@hotmail.com>
2018-04-16 13:52:57 +0200

    Update configs from linux script

--- .oh-my-zsh/custom/greet.sh
+++ .oh-my-zsh/custom/greet.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+# Script to print greeting to user in the center of the console window
+
+hour=$(date +"%H")
+
+if [ $hour -ge 0 -a $hour -lt 12 ]
+then
+    text="Good Morning, $USER"
+elif [ $hour -ge 12 -a $hour -lt 18 ]
+then
+    text="Good Afternoon, $USER"
+else
+    text="Good evening, $USER"
+fi
 
 COLUMNS=$(tput cols)
 echo
 echo
-text="Good morning, $(whoami)."
 printf "%*s\n" $(((${#text}+$COLUMNS)/2)) "$text"
 
